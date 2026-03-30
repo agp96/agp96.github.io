56e8689ddc1d055ce9e23683c2a5d1585ffa65a9
agp08 <agpalmoradi@hotmail.com>
2026-03-30 20:32:38 +0200

    Update input.js

--- js/input.js
+++ js/input.js
@@ -1,40 +1,40 @@
 
-(function() {
+(function () {
     var pressedKeys = {};
 
     function setKey(event, status) {
-        var code = event.keyCode;
+        var code = event.key;
         var key;
 
-        switch(code) {
-        case 75:
-            key = 'SHOOT'; break;
-        case 87:
-            key = 'UP'; break;
-        case 83:
-            key = 'DOWN'; break;
-        default:
-            // Convert ASCII codes to letters
-            key = String.fromCharCode(code);
+        switch (code) {
+            case 'k':
+                key = 'SHOOT'; break;
+            case 'w':
+                key = 'UP'; break;
+            case 's':
+                key = 'DOWN'; break;
+            default:
+                // Convert ASCII codes to letters
+                key = String.fromCharCode(code);
         }
 
         pressedKeys[key] = status;
     }
 
-    document.addEventListener('keydown', function(e) {
+    document.addEventListener('keydown', function (e) {
         setKey(e, true);
     });
 
-    document.addEventListener('keyup', function(e) {
+    document.addEventListener('keyup', function (e) {
         setKey(e, false);
     });
 
-    window.addEventListener('blur', function() {
+    window.addEventListener('blur', function () {
         pressedKeys = {};
     });
 
     window.input = {
-        isDown: function(key) {
+        isDown: function (key) {
             return pressedKeys[key.toUpperCase()];
         }
     };
