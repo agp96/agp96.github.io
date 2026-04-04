7a450d3544a8f2bd46a733b1e058a79fcc5c10b9
agp08 <agpalmoradi@hotmail.com>
2026-04-04 13:41:05 +0200

    Update input.js

--- js/input.js
+++ js/input.js
@@ -37,4 +37,26 @@
             return pressedKeys[key.toUpperCase()];
         }
     };
+
+    // Controles táctiles
+    const touchMap = {
+        'btn-up':    'UP',
+        'btn-down':  'DOWN',
+        'btn-shoot': 'SHOOT'
+    };
+    
+    Object.keys(touchMap).forEach(function(id) {
+        const btn = document.getElementById(id);
+        const key = touchMap[id];
+    
+        btn.addEventListener('touchstart', function(e) {
+            e.preventDefault();
+            pressedKeys[key] = true;
+        }, { passive: false });
+    
+        btn.addEventListener('touchend', function(e) {
+            e.preventDefault();
+            pressedKeys[key] = false;
+        }, { passive: false });
+    });
 })();
